d05f9348975e83a27b4a5b843a400529c34649da
amar-ai <67940540+amar-ai@users.noreply.github.com>
2021-03-12 21:01:13 +1100

    #First push, adding files for creating polts and correlations

---
+++ correlations.R
@@ -0,0 +1,32 @@
+library(Lahman)
+library(tidytext)
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+
+##Relationship between at bats per game and runs per game
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  mutate(AB_per_game = AB/G, R_per_game = R/G) %>%
+  ggplot(aes(AB_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  mutate(AB_per_game = AB/G, R_per_game = R/G) %>%
+  summarise(cor(AB_per_game,R_per_game))
+
+##Relationship between win rate per game and fielding errors per game
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  mutate(E_per_game = E/G, W_per_game = W/G) %>%
+  ggplot(aes(E_per_game, W_per_game)) + 
+  geom_point(alpha = 0.5)
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  mutate(E_per_game = E/G, W_per_game = W/G) %>%
+  summarise(cor(E_per_game,W_per_game))
+
+##Relationship between triples per game vs doubles per gameTeams %>% filter(yearID %in% 1961:2001 ) %>%
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  mutate(X3B_per_game = X3B/G, X2B_per_game = X2B/G) %>%
+  ggplot(aes(X2B_per_game, X3B_per_game)) + 
+  geom_point(alpha = 0.5)
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  mutate(X3B_per_game = X3B/G, X2B_per_game = X2B/G) %>%
+  summarise(cor(X3B_per_game,X2B_per_game))
